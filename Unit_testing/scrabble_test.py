2b640f6f1a925d770be9cb151c637e7a38374557
Alex <ASam@spartaglobal.com>
2020-07-16 00:55:53 +0100

    "Added pool size test to tile_hand and combined tile_hand and generate_hand into one function"

--- Unit_testing/scrabble_test.py
+++ Unit_testing/scrabble_test.py
@@ -31,10 +31,26 @@ class Scrabbletest(unittest.TestCase):
         self.assertEqual(self.game.calculate_score("oxyphenbutazone"), 41)
         self.assertEqual(self.game.calculate_score(""), None)
 
-# It will return a list of 7 random letters from a pool, 7 letters that are alphabetical
+# It will return a list of 7 random letters from a pool, 7 letters that are alphabetical, also tests the pool size
     def test_generate_tile_hand(self):
-        self.assertEqual(len(self.game.generate_tile_hand()), 7)
-        self.assertTrue(self._is_list_isalpha(self.game.generate_tile_hand()))
+        self.assertEqual(len(self.game.tile_hand(["A", "B", "C", "D", "E", "F"])), 7)
+        self.assertEqual(len(self.game.tile_hand([])), 7)
+        self.assertTrue(self._is_list_isalpha(self.game.tile_hand(["a", "b", "c", "d", "e"])))
+        self.assertTrue(self._is_list_isalpha(self.game.tile_hand([])))
+
+        scrabble = ScrabbleGame()
+        pool_size = 98
+        scrabble.tile_hand([])
+        self.assertEqual(pool_size - 7, sum(scrabble.scrabble_pool.values()))
+        pool_size = sum(scrabble.scrabble_pool.values())
+        scrabble.tile_hand(["a", "b", "c", "d", "e"])
+        self.assertEqual(pool_size - 2, sum(scrabble.scrabble_pool.values()))
+
+        scrabble2 = ScrabbleGame()
+        scrabble2.scrabble_pool = {"A": 4}
+        returned_hand = scrabble2.tile_hand([])
+        self.assertEqual(len(returned_hand), 4)
+        self.assertEqual(0, sum(scrabble2.scrabble_pool.values()))
 
     def test_is_valid_word(self):
         self.assertTrue(self.game.is_valid_word("animal", ["test", "case", "does", "it", "work", "animal"]))
@@ -42,27 +58,8 @@ class Scrabbletest(unittest.TestCase):
         self.assertFalse(self.game.is_valid_word("animal", [1, 2, 3, 4, 5]))
         self.assertIsNone(self.game.is_valid_word("animal", []))
 
-    def test_replace_tiles(self):
-        replaced_tiles = self.game.replace_tiles(["a", "b", "c", "d", "e"])
-        self.assertTrue(self._is_list_isalpha(replaced_tiles))
-        self.assertTrue(self._is_list_isalpha(self.game.replace_tiles(["A", "B", "C", "D", "E", "F"])))
-        self.assertTrue(self._is_list_isalpha(self.game.replace_tiles([])))
-        self.assertEqual(len(self.game.replace_tiles(replaced_tiles)), 7)
-
-    # def test_calculate_best_word(self):
 
     def test_shuffle_hand(self):
         self.assertTrue(self.game.shuffle_hand(["A", "B", "C", "D", "E", "F", "G"]))
 
-
-# if word is in generate tile hand,
-# if the word exists
-# input : word to test
-# input : list of words
-# output : boolean
-
-
-# once a word has been made, replace only the used tiles # care
-# input = string (ASD____)
-# output = (ASDHJKL)
 
