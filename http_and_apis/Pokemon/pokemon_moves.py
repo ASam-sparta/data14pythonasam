3652532cfce9bcca28d5445d6c7781cc2586565f
Alex <ASam@spartaglobal.com>
2020-07-19 00:32:39 +0100

    "Created class for Pokemon moves with init"

---
+++ http_and_apis/Pokemon/pokemon_moves.py
@@ -0,0 +1,29 @@
+import requests
+from pprint import pprint
+
+class PokemonMoves:
+
+    def __init__(self, moves_json):
+        self.accuracy = moves_json['accuracy']
+        self.damage_class = moves_json['damage_class']
+        self.effect_chance = moves_json['effect_chance']
+        self.effect_entries = moves_json['effect_entries']
+        self.short_effect = self.effect_entries['short_effect']
+        self.generation = moves_json['generation']
+        self.id = moves_json['id']
+        self.machines = moves_json['machines']
+        self.meta = moves_json['meta']
+        self.name = moves_json['name']
+        self.power = moves_json['power']
+        self.pp = moves_json['pp']
+        self.priority = moves_json['priority']
+        self.stat_changes = moves_json['stat_changes']
+        self.target = moves_json['target']
+        self.type = moves_json['type']
+
+url = "https://pokeapi.co/api/v2/move/144/"
+request = requests.get(url)
+header_details = request.headers
+response_json = request.json()
+
+pprint(response_json)
