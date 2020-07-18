8183c8624e5c924db172821603c71fedbf58fab6
Alex <ASam@spartaglobal.com>
2020-07-19 00:19:24 +0100

    "Created class for Pokemon with init"

---
+++ http_and_apis/Pokemon/pokemon_model.py
@@ -0,0 +1,34 @@
+import requests
+from pprint import pprint
+
+class Pokemon:
+
+    def __init__(self, pokemon_json):
+        self.status = pokemon_json['status']
+        self.abilities = pokemon_json['abilities']
+        self.base_experience = pokemon_json["base_experience"]
+        self.forms = pokemon_json['forms']
+        self.game_indices = pokemon_json['game_indices']
+        self.height = pokemon_json['height']
+        self.held_items = pokemon_json['held_items']
+        self.id = pokemon_json['id']
+        self.is_default = pokemon_json['is_default']
+        self.location_area_encounters = pokemon_json['pokemon_area_encounters']
+        self.moves = pokemon_json['moves']
+        self.name = pokemon_json['name']
+        self.order = pokemon_json['order']
+        self.species = pokemon_json['species']
+        self.sprites = pokemon_json['sprites']
+        self.stats = pokemon_json['stats']
+        self.types = pokemon_json['types']
+        self.weight = pokemon_json['weight']
+    def ability(self):
+        return "abc"
+
+
+url = "https://pokeapi.co/api/v2/pokemon/ditto"
+request = requests.get(url)
+header_details = request.headers
+response_json = request.json()
+
+pprint(response_json)
